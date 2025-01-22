0f0f4e70704110b03fe6c97f9828585dae82cdca
FantomeBeignet <himself@fantomebeig.net>
2025-01-23 00:37:58 +0100

    build: downgrade to go 1.22

--- nix/shell.nix
+++ nix/shell.nix
@@ -13,7 +13,10 @@
   mkGoEnv ? pkgs.mkGoEnv,
   gomod2nix ? pkgs.gomod2nix,
 }: let
-  goEnv = mkGoEnv {pwd = ../.;};
+  goEnv = mkGoEnv {
+    pwd = ../.;
+    go = pkgs.go_1_22;
+  };
 in
   pkgs.mkShell {
     packages = with pkgs; [
